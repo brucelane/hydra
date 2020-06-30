dd0a274ad5c248d8ca84005ff7869128a3b84f57
BruceLane <bruce@batchass.fr>
2020-07-01 00:05:33 +0200

    try 2

--- hydra-server/server.js
+++ hydra-server/server.js
@@ -22,6 +22,6 @@ server.listen(8000, function () {
 })
 
 app.use(express.static(path.join(__dirname, '/public')))
-app.use(express.static('/public'))
-server.use(express.static(path.join(__dirname, '/public')))
-console.log('end')
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../../public')))
+app.use(express.static(path.join(__dirname, '../../../../public')))
