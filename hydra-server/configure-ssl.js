7063007abaf73f49a9927013527cae9fc8e5eb69
BruceLane <bruce@batchass.fr>
2020-07-02 00:22:42 +0200

    configure-ssl.js

---
+++ hydra-server/configure-ssl.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+
+// if on glitch, force https
+module.exports = (app) => {
+  var server
+  if(process.env.GLITCH) {
+    var http = require('http')
+    server = http.createServer(app)
+
+    function checkHttps(req, res, next){
+    if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+       // console.log("https, yo")
+        return next()
+      } else {
+        res.redirect('https://' + req.hostname + req.url);
+      }
+    }
+
+    app.all('*', checkHttps)
+  } else {
+    var https = require('https')
+    //var privateKey = fs.readFileSync(path.join(__dirname, '/certs/key.pem'), 'utf8')
+    //var certificate = fs.readFileSync(path.join(__dirname, '/certs/certificate.pem'), 'utf8')
+    var privateKey = fs.readFileSync('/etc/letsencrypt/live/sophiadigitalart.fr/privkey.pem', 'utf8')
+    var certificate = fs.readFileSync('/etc/letsencrypt/live/sophiadigitalart.fr/fullchain.pem', 'utf8')
+    var credentials = {key: privateKey, cert: certificate}
+    server = https.createServer(credentials, app)
+  }
+  return server
+}
